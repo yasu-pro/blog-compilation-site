7ed6d0db97a857a0521d806228b6644c1ba91058
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-10-01 21:53:02 +0900

    Fix: Fix rendering when props posts value is changed

--- src/components/BlogArchive.tsx
+++ src/components/BlogArchive.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import type { Posts } from "../types/types";
 import BlogItem from "./BlogItem";
-import Styles from "../styles/scss/components/BlogArchive.module.scss"
-
-
+import Styles from "../styles/scss/components/BlogArchive.module.scss";
 
 const BlogArchive: React.FC<Posts> = ({ posts }) => {
+    const [getPosts, setGetPosts] = useState(posts);
+    useEffect(() => {
+        setGetPosts(posts);
+    }, [posts]);
 
     return (
         <div className={Styles.blogContents}>
             <div className={Styles.blogContents__inner}>
-                {posts.map((post, index) => (
-                    <BlogItem key={ index } post={ post.node } />
+                {getPosts.map((post, index) => (
+                    <BlogItem key={index} post={post.node} />
                 ))}
             </div>
         </div>
