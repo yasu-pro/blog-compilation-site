6cc2e3eccbc828d3562dcf634b9fe57730ce1247
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-10-03 15:42:46 +0900

    Fix: removed useEffect from BlogArchive

--- src/components/BlogArchive.tsx
+++ src/components/BlogArchive.tsx
@@ -4,15 +4,10 @@ import BlogItem from "./BlogItem";
 import Styles from "../styles/scss/components/BlogArchive.module.scss";
 
 const BlogArchive: React.FC<Posts> = ({ posts }) => {
-    const [getPosts, setGetPosts] = useState(posts);
-    useEffect(() => {
-        setGetPosts(posts);
-    }, [posts]);
-
     return (
         <div className={Styles.blogContents}>
             <div className={Styles.blogContents__inner}>
-                {getPosts.map((post, index) => (
+                {posts.map((post, index) => (
                     <BlogItem key={index} post={post.node} />
                 ))}
             </div>
