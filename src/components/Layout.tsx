6b4f30a2971952de538b78934149bc9c817ed64a
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-10-15 11:52:56 +0900

    Fix: Fixed to set values in parent component and pass them to child components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,11 +3,16 @@ import Header from './Header';
 import Footer from './Footer';
 import PageTitle from '../components/PageTitle';
 
-const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+interface LayoutProps {
+  children?: React.ReactNode;
+  title: string;
+}
+
+const Layout: React.FC< LayoutProps > = ({ children, title }) => {
   return (
     <main>
       <Header />
-      <PageTitle title='Blog Compilation Site' />
+      <PageTitle title={title} />
       {children}
       <Footer />
     </main>
