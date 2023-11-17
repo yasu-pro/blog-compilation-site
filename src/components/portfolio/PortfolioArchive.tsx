7a24c6900cc76414b27edc3185eb655aa3f5e010
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-11-17 13:54:16 +0900

    Add: Added css to portfolio page

--- src/components/portfolio/PortfolioArchive.tsx
+++ src/components/portfolio/PortfolioArchive.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Attributes } from '../../graphql/types/strapiType';
 import PortfolioItem from './PortfolioItem'
+import Styles from '../../styles/scss/pages/portfolio/portfolioArchive.module.scss';
 
 const PortfolioArchive: React.FC<{ posts: Attributes[] }> = ({ posts }) => {
     return (
-        <div>
-            <div>
+        <div className={ Styles.portfolioContents }>
+            <section className={ Styles.portfolioSection }>
                 {posts.map((post, index) => (
                     <PortfolioItem key={index} post={post.attributes} />
                 ))}
+            </section>
+
+            <div>
+                <a href="#">prev</a>
+                <a href="#">一覧</a>
+                <a href='#'>next</a>
             </div>
         </div>
     );
